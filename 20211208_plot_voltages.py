e65a0f8ad34166a70a0103d4019580df9af55944
dbnyu <65036353+dbnyu@users.noreply.github.com>
2021-12-09 11:47:45 -0500

    new file

---
+++ 20211208_plot_voltages.py
@@ -0,0 +1,15 @@
+"""Data analysis from silicone phantom experiments 12/08/2021.
+
+    Recorded single pulse/acquisition with different setups
+    with (water?) filled silicone phantom; no other objects.
+
+    Used 10x sine wave packet and single rectangular pulse.
+
+    TODO - 1usec duration rectangular pulse may be wrong- it might 
+            need to be 0.5usec == 1/2 period of transducer center frequency...
+            DOUBLE CHECK THIS!
+"""
+
+
+
+
