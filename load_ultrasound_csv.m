944daac01a5d5d4385cf8e2ee5d15514dc2c6d2f
dbnyu <65036353+dbnyu@users.noreply.github.com>
2021-12-21 12:36:03 -0500

    first working version

--- load_ultrasound_csv.m
+++ load_ultrasound_csv.m
@@ -1,14 +1,34 @@
-function A = load_ultrasound_csv(filename)
+function A = load_ultrasound_csv(filename, Nchannels, int16)
 % Load an ultrasound CSV file recorded w/ Digilent device
+%	filename = path to file
+%	Nchannels = # of channels (1 or 2)
+%	int16	= True if data is raw int16 or not
 
+
+
+if exist('int16', 'var') ~= 2
+	int16 = false;
+end
+
+fprintf('Loading %s\n', filename)
 A = readtable(filename);
 
+if int16
+	v_header = 'int16';
+else
+	v_header = 'volts';
+end
 
-A = renamevars(A, ["Var1", "Var2", "Var3"], ["Index", "Time", "Voltage"]);
 
-%setColHeading(A, 1, 'Index')
-%setColHeading(A, 2, 'Time')
-%setColHeading(A, 3, 'Voltage')
+ch1header = ['ch1_', v_header];
+ch2header = ['ch2_', v_header];	% not used in 1ch case
+
+
+if Nchannels == 1
+	A = renamevars(A, ["Var1", "Var2", "Var3"], ["Index", "Time", ch1header]);
+else
+	A = renamevars(A, ["Var1", "Var2", "Var3", "Var4"], ["Index", "Time", ch1header, ch2header]);
+end
 
 
 fprintf('Read in size:\n')
