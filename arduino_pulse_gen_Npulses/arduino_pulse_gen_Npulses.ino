a71827775009710d7fb0245caff6b326c48feede
dbnyu <65036353+dbnyu@users.noreply.github.com>
2022-02-28 18:59:33 -0500

    copied v1, this always does a fixed # of pulses for easier debugging

--- arduino_pulse_gen_Npulses/arduino_pulse_gen_Npulses.ino
+++ arduino_pulse_gen_Npulses/arduino_pulse_gen_Npulses.ino
@@ -1,5 +1,9 @@
 /* Pulse Generator to test AD2 Trigger/Timestamp recorder.
  * 
+ * Version 2.0 - Output a fixed number of pulses when switch is activated
+ *  - to test the trigger count/look for false triggers
+ *
+ *
  * Generate a square pulse on one pin.
  * And additional synchronous LED pulse on separate pin.
  * 
@@ -10,25 +14,35 @@
  * - https://www.arduino.cc/en/pmwiki.php?n=Reference/PortManipulation
  * - https://www.baldengineer.com/six-oscilloscope-measurements-using-arduino.html
  * 
- * Doug Brantner 2/25/2022
+ * Doug Brantner 2/28/2022
  */
+
+// TODO BUGS TO FIX IN FIRST VERSION:
+// - subtract pulse width from TR, for more accurate pulse timing
+
 
 
 // NOTE: for delayMicroseconds() - 
 // Largest value that will produce an accurate delay is 16383
+// 1 usec delay seems too small (~0.32usec on scope)
+// 10 usec delay seems ok (slightly less than 10us but useable)
 
 // TODO for LED to be visible we may need LED duration > Pulse Width...
 
 long TR_DELAY = 2000;  // microseconds between pulses
 // average expected ~2-4 milliseconds MRI TR (2000-4000 usec)
 
-long PULSE_WIDTH = 1; // microseconds for high pulse
+long PULSE_WIDTH = 10; // microseconds for high pulse
 // NOTE: with this = 1, Rigol scope shows actual pulse with ~0.320 usec...
+
+
+int N_PULSES = 1000;  // output a fixed number of pulses when momentary switch is closed
         
 
 // TODO make these in the same block so we can just write both at once (bitwise/registerwise)
 int TRIGGER_OUT_PIN = 8; // PORTB least significant bit 0000 0001
 int LED_PIN = 13;  // PORTB  0010 0000
+int START_SWITCH_PIN = 2; // momentary switch to start fixed count pulse train
 
 // NOTE: BE CAREFUL WITH PORT B:
 // PORTB maps to Arduino digital pins 8 to 13 The two high bits (6 & 7) map to the crystal pins and are not usable 
@@ -37,9 +51,11 @@ int LED_PIN = 13;  // PORTB  0010 0000
 char PIN_8_ON  = B00000001;
 char PIN_13_ON = B00100000;
 
+char PIN_8_OFF = ~PIN_8_ON;
+
 char LED_AND_TRIGGER_ON = PIN_8_ON | PIN_13_ON; // both on at the same time
       // this should look like 0010 0001
-char LED_AND_TRIGGER_OFF = !LED_AND_TRIGGER_ON; // both off; everything else ON
+char LED_AND_TRIGGER_OFF = ~LED_AND_TRIGGER_ON; // both off; everything else ON
       // BE CAREFUL WITH HIGHEST 2 BITS!! see above.
       // this should look like 1101 1110
 
@@ -47,28 +63,41 @@ char LED_AND_TRIGGER_OFF = !LED_AND_TRIGGER_ON; // both off; everything else ON
 void setup() {
   // put your setup code here, to run once:
 
+  pinMode(START_SWITCH_PIN, INPUT_PULLUP);
   pinMode(TRIGGER_OUT_PIN, OUTPUT);
   pinMode(LED_PIN, OUTPUT);
 
-  // TODO pullups? add resistors on outputs?
 
   digitalWrite(TRIGGER_OUT_PIN, LOW);
   digitalWrite(LED_PIN, LOW);
 
+  // adjust TR delay for pulse width:
+  TR_DELAY -= PULSE_WIDTH;
 }
+
+int pulses = 0; // how many pulses are emitted ('for' loop variable)
 
 void loop() {
   // put your main code here, to run repeatedly:
 
-  //digitalWrite(TRIGGER_OUT_PIN, HIGH);
-  //digitalWrite(LED_PIN, HIGH);
-  PORTB = PORTB | LED_AND_TRIGGER_ON;
-  
-  delayMicroseconds(PULSE_WIDTH);
-  
-  //digitalWrite(TRIGGER_OUT_PIN, LOW);
-  //digitalWrite(LED_PIN, LOW);
-  PORTB = PORTB & LED_AND_TRIGGER_OFF;
+  if (!digitalRead(START_SWITCH_PIN)) {
+    // only checking once... hopefully we dont need to debounce beyond that...
 
-  delayMicroseconds(TR_DELAY);
+    digitalWrite(LED_PIN, HIGH);  // turn on LED for full duration of pulses
+
+    for (pulses = 0; pulses < N_PULSES; pulses++) {
+      //PORTB = PORTB | LED_AND_TRIGGER_ON;
+      PORTB = PORTB | PIN_8_ON; // only switch the signal pin; LED is on for the full duration now.
+
+      delayMicroseconds(PULSE_WIDTH);
+
+      //PORTB = PORTB & LED_AND_TRIGGER_OFF;
+      PORTB = PORTB & PIN_8_OFF;
+
+      delayMicroseconds(TR_DELAY);
+    }
+
+    digitalWrite(LED_PIN, LOW);
+  }
+
 }
